fc3671615d207cfa2b999cecc13a1694e6e03c5d
Jose Murillo <jp1995azul@hotmail.com>
2021-02-11 23:40:59 -0600

    Adding html loader and webpack plugin to process html files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.html/i,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            minimize: true,
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+    ],
+};
